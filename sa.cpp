68ebf38b65bce6fe61952505941bdc79ca081073
MauricioMancillaTusco <mauricio.mancilla@sistemas.edu.bo>
2025-06-05 00:02:14 -0400

    Add files via upload

---
+++ sa.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+       cout<<"hola "<<endl;
+       for (int i = 1; i <= 12; i++)
+       {
+         cout<<i<<endl;
+
+
+
+
+       }
+       return 0;
+}
+
